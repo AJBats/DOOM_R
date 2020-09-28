daa3695fe28023e0174fece82990497ff9939a59
AJBats <albatsford@gmail.com>
2020-09-27 22:56:44 -0400

    Adding ignore file, and dummyfile.h which is home to file IO dummy functions.

---
+++ sh2doom-1.10/dummyfile.h
@@ -0,0 +1,31 @@
+
+#ifndef _DUMMY_FILE_H_
+#define _DUMMY_FILE_H_
+
+__BEGIN_DECLS
+
+#define FILE int
+
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+static inline FILE* __always_inline 
+fopen(char const* _FileName, char const* _Mode)
+{
+    return NULL;
+}
+
+static inline int __always_inline 
+fprintf(FILE* const _Stream, char const* const _Format, ...)
+{
+    return 0;
+}
+
+static inline int __always_inline 
+fclose(FILE* _Stream)
+{
+    return 0;
+}
+#pragma GCC diagnostic pop
+
+__END_DECLS
+
+#endif /* !_DUMMY_FILE_H_ */
